6952db2041905c74b346b7d2ea1aabc97c187011
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2023-10-14 22:49:42 +0600

    Improvements and Optimizations

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
+const tableHead = document.getElementById('data-head');
 const tableBody = document.getElementById('data-body');
 const searchInput = document.getElementById('searchInput');
 let jsonData = []; // To store data from all .json files
 //row number function
 
-function populateTable(data) {
-    tableBody.innerHTML = '';
-    let counter = 1;
+function populateTable(data, searched = false) {
+    tableHead.innerHTML = `
+        <tr>
+            ${searched ? '<th>ID</th>' : ''}
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Team Name</th>
+            <th>Institution</th>
+            <th>Solved</th>
+            <th>Problems</th>
+        </tr>
+    `;
 
-    data.forEach(item => {
+    tableBody.innerHTML = '';
+
+    data.forEach((item, idx) => {
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td>${counter}</td>
+           ${searched ? `<th>${idx + 1}</th>` : ''}
+            <td>${item.rank}</td>
             <td>${item.username}</td>
             <td>${item.fullname}</td>
             <td>${item.institution}</td>
-            <td>${item.total_fine}</td>
-            <td>${item.problem_total_points}</td>
+            <td>${item.problem_total_points} (${item.total_fine})</td>
+            <td>${item.problem_list.filter(i => i.is_solved).map(i => i.number_to_alpha).join(' ')}</td>
         `;
         tableBody.appendChild(row);
-        counter++;
     });
 }
 
 // Fetch and combine data from all .json files
 async function fetchAndCombineData() {
-    jsonData = [];
-
-    for (let page = 1; page <= 13; page++) {
-        const response = await fetch(`${page}.json`);
-        const data = await response.json();
-        jsonData = jsonData.concat(data.results);
-    }
-
+    jsonData = await fetch('data.json').then(res => res.json());
+    jsonData = jsonData.map((item, idx) => ({ ...item, rank: idx + 1 }));
     populateTable(jsonData);
 }
 
 fetchAndCombineData();
 
 searchInput.addEventListener('input', () => {
-    const searchTerm = searchInput.value.toLowerCase();
-    const filteredData = jsonData.filter(item => 
+    const searchTerm = searchInput.value.toLowerCase().replace(/\s+/g, ' ');
+    if (!searchTerm) {
+        populateTable(jsonData);
+        return;
+    }
+
+    const filteredData = jsonData.filter(item =>
         item.username.toLowerCase().includes(searchTerm) ||
         item.fullname.toLowerCase().includes(searchTerm) ||
         item.institution.toLowerCase().includes(searchTerm)
     );
-    populateTable(filteredData);
+    populateTable(filteredData, true);
 });

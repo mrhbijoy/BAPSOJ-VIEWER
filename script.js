94bfdaf785ea1c83afe11013fcc7f2ab7ccd0229
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2023-10-16 06:26:00 +0600

    remove innerHTML usage and improve search typing

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-const tableHead = document.getElementById('data-head');
 const tableBody = document.getElementById('data-body');
 const searchInput = document.getElementById('searchInput');
 let jsonData = []; // To store data from all .json files
 //row number function
 
-function populateTable(data, searched = false) {
-    tableHead.innerHTML = `
-        <tr>
-            ${searched ? '<th>ID</th>' : ''}
-            <th>Rank</th>
-            <th>Username</th>
-            <th>Team Name</th>
-            <th>Institution</th>
-            <th>Solved</th>
-            <th>Problems</th>
-        </tr>
-    `;
+function addCell(row, content, th = false) {
+    const cell = document.createElement(th ? 'th' : 'td');
+    cell.appendChild(document.createTextNode(content));
+    row.appendChild(cell);
+}
 
-    tableBody.innerHTML = '';
+function populateTable(data, searched = false) {
+    const idColumn = document.getElementById('column-id');
+    idColumn.classList.toggle('hidden', !searched);
+
+    // remove all elements from table body
+    while (tableBody.firstChild) {
+        tableBody.removeChild(tableBody.firstChild);
+    }
 
     data.forEach((item, idx) => {
         const row = document.createElement('tr');
-        row.innerHTML = `
-           ${searched ? `<th>${idx + 1}</th>` : ''}
-            <td>${item.rank}</td>
-            <td>${item.username}</td>
-            <td>${item.fullname}</td>
-            <td>${item.institution}</td>
-            <td>${item.problem_total_points} (${item.total_fine})</td>
-            <td>${item.problem_list.filter(i => i.is_solved).map(i => i.number_to_alpha).join(' ')}</td>
-        `;
+
+        if (searched) addCell(row, idx + 1, true);
+        addCell(row, item.rank, !searched);
+        addCell(row, item.username);
+        addCell(row, item.fullname);
+        addCell(row, item.institution);
+        addCell(row, `${item.problem_total_points} (${item.total_fine})`);
+        addCell(row, item.problem_list.filter(i => i.is_solved).map(i => i.number_to_alpha).join(' '));
+
         tableBody.appendChild(row);
     });
 }
@@ -43,17 +42,22 @@ async function fetchAndCombineData() {
 
 fetchAndCombineData();
 
+let searchTimeout = null;
 searchInput.addEventListener('input', () => {
-    const searchTerm = searchInput.value.toLowerCase().replace(/\s+/g, ' ');
-    if (!searchTerm) {
-        populateTable(jsonData);
-        return;
-    }
+    searchTimeout = setTimeout(() => {
+        clearTimeout(searchTimeout);
 
-    const filteredData = jsonData.filter(item =>
-        item.username.toLowerCase().includes(searchTerm) ||
-        item.fullname.toLowerCase().includes(searchTerm) ||
-        item.institution.toLowerCase().includes(searchTerm)
-    );
-    populateTable(filteredData, true);
+        const searchTerm = searchInput.value.toLowerCase().replace(/\s+/g, ' ');
+        if (!searchTerm) {
+            populateTable(jsonData);
+            return;
+        }
+
+        const filteredData = jsonData.filter(item =>
+            item.username.toLowerCase().includes(searchTerm) ||
+            item.fullname.toLowerCase().includes(searchTerm) ||
+            item.institution.toLowerCase().includes(searchTerm)
+        );
+        populateTable(filteredData, true);
+    }, 300);
 });
